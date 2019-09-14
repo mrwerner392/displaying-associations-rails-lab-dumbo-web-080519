7e78294a39074e9a9c73e695302b901b3b89a49e
mrwerner392 <mrwerner392@gmail.com>
2019-09-14 15:39:31 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.destroy_all
+Song.destroy_all
+@artist = Artist.create!(name: "Daft Punk")
+@song = @artist.songs.create!(title: "The Grid")
